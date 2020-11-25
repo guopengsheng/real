47baa14335c8be9b5bda7332f4f385256e0631f4
zhangtong_glb <zhangtong_glb@kgc.com>
2020-11-25 17:22:25 +0800

    yuzepengtets03

--- real/src/main/java/com/gps/spring/controller/UserController.java
+++ real/src/main/java/com/gps/spring/controller/UserController.java
@@ -15,5 +15,37 @@ import java.util.List;
 public class UserController {
     @Autowired
     UserService userService;
+    @RequestMapping("login")
+    public String login(String cardId,String password, HttpSession session,Model model){
+        List<User> users = userService.login(cardId);
+        if (users!=null && users.size()>0){
+            if (users.get(0).getPassword().equals(password)){
+                session.setAttribute("users",users.get(0));
+                return "homepage";
+            }else {
+                model.addAttribute("msg","密码错误");
+                return "login";
+            }
+        }else {
+            model.addAttribute("msg","用户不存在");
+            return "login";
+        }
+    }
+    @RequestMapping("register")
+    public String register(String cardId,String name,String password,Model model){
+        User user = new User();
+        user.setCardid(cardId);
+        user.setName(name);
+        user.setPassword(password);
+        user.setCreatetime(new Date());
+        Boolean register = userService.register(user);
+        if (register){
+            model.addAttribute("msg","注册成功");
+            return "login";
+        }else {
+            model.addAttribute("msg","注册失败");
+            return "register";
+        }
+    }
 
 }
