48c5c18bd567ef3b4f959f5458946be998e074e4
zhangtong_glb <zhangtong_glb@kgc.com>
2020-11-25 17:24:59 +0800

    xbntets01

--- real/src/main/java/com/gps/spring/controller/RealEstateController.java
+++ real/src/main/java/com/gps/spring/controller/RealEstateController.java
@@ -16,5 +16,31 @@ import java.util.List;
 public class RealEstateController {
     @Autowired
     RealEstateService realEstateService;
-
+    @RequestMapping("page")
+    public String page(Integer type,String likename,Model model,Integer pageNum,Integer pageSize){
+        Integer type1 =0;
+        if (type!=null){
+            type1 = type;
+            model.addAttribute("type",type1);
+        }
+        String likename1 ="";
+        if (likename!=null){
+            likename1 = likename;
+            model.addAttribute("likename",likename1);
+        }
+        Integer pageNum1 =1;
+        if (pageNum!=null){
+            pageNum1 = pageNum;
+        }
+        Integer pageSize1 = 3;
+        if (pageSize!=null){
+            pageSize1 = pageSize;
+        }
+        PageInfo<RealEstate> pageInfo = realEstateService.selectAll(type1, likename1, pageNum1, pageSize1);
+        if (pageInfo.getList()==null || pageInfo.getList().size()<=0){
+            model.addAttribute("msg","对不起，没有符合条件的查询");
+        }
+        model.addAttribute("pageInfo",pageInfo);
+        return "page";
+    }
 }
